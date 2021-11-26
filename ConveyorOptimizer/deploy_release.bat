350d303866b9924bb120cc9bc2caf0920307aab7
Viktor Ferenczi <viktor@ferenczi.eu>
2021-11-26 21:25:58 +0100

    0.2.3 (#1)
    Fixed crash on invoking !conveyor info command from the Torch GUI instead of in-game
    
    Fixed description: !help conveyor

--- ConveyorOptimizer/deploy_release.bat
+++ ConveyorOptimizer/deploy_release.bat
@@ -1,4 +1,4 @@
-md "..\..\TorchDir\Plugins\ConveyorOptimizer" 2>NUL
-copy /y "bin\Release\manifest.xml" "..\..\TorchDir\Plugins\ConveyorOptimizer\"
-copy /y "bin\Release\ConveyorOptimizer.pdb" "..\..\TorchDir\Plugins\ConveyorOptimizer\"
-copy /y "bin\Release\ConveyorOptimizer.dll" "..\..\TorchDir\Plugins\ConveyorOptimizer\"
+md "..\TorchDir\Plugins\ConveyorOptimizer" 2>NUL
+copy /y "bin\Release\manifest.xml" "..\TorchDir\Plugins\ConveyorOptimizer\"
+copy /y "bin\Release\ConveyorOptimizer.pdb" "..\TorchDir\Plugins\ConveyorOptimizer\"
+copy /y "bin\Release\ConveyorOptimizer.dll" "..\TorchDir\Plugins\ConveyorOptimizer\"
